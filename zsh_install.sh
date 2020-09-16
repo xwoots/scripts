bbdaa92a5bb5dda723e64f078480757580e53728
xwoot <57961128+xwoot@users.noreply.github.com>
2020-09-17 00:23:22 +0200

    Delete zsh_install.sh

--- zsh_install.sh
+++
@@ -1,6 +0,0 @@
-# Install commons packages
-sudo apt update
-sudo apt install -y curl git wget zsh fonts-powerline
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
