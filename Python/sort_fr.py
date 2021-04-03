7746f9f9aecd739d9a3daadbcdbe484ff323542b
xwoot <mateuw@goelan.znetwork.local>
2021-04-03 22:44:06 +0200

    03042021_2

--- Python/sort_fr.py
+++ Python/sort_fr.py
@@ -15,30 +15,32 @@ os.chdir(path)
 list = os.listdir(os.getcwd())
 username = os.getenv('USER')
 
+def move_file(dir):
+	global username
+	global path
+	try:
+		shutil.move(filename, f'/home/{username}/{dir}')
+	except shutil.Error:
+		print(f'Le fichier existe déjà : /home/mateuw/{dir}/{filename}')
+		rem_file = input("Supprimer le fichier ? [y/N] ")
+		if rem_file == "y":
+			os.remove(f'{path}/{filename}')
+
 for filename in list:
 	if filename.endswith(pictures_ext):
-		shutil.move(filename, f'/home/{username}/Images')
-	
+		move_file("Images")	
 	elif filename.endswith(videos_ext):
-		shutil.move(filename, f'/home/{username}/Vidéos')
-	
+		move_file("Vidéos")	
 	elif filename.endswith(texts_ext):
-		shutil.move(filename, f'/home/{username}/Documents')
-	
+		move_file("Documents")		
 	elif filename.endswith(musics_ext):
-		shutil.move(filename, f'/home/{username}/Musique')
-	
+		move_file("Musique")
 	elif filename.endswith(torrents_ext):
-		shutil.move(filename, f'/home/{username}/Torrents')
-	
+		move_file("Torrents")
 	elif filename.endswith(ISOs_ext):
-		shutil.move(filename, f'/home/{username}/ISOs')
-	
+		move_file("ISOs")
 	elif filename.endswith(scripts_ext):
-		shutil.move(filename, f'/home/{username}/Scripts')
+		move_file("Scripts")
 
-try: 
-	shutil.move(filename, '/home/{}/Images'.format(username))
-except: 
-	print("Le fichier {} existe déjà".format(filename))
 
+	
