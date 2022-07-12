32f519163aef99c0e489d007f19cddbdfde2e8a9
xwoot <mateuw.coulibaly@gmail.com>
2022-07-12 18:48:28 +0200

    12072022_2

--- Python/json2excel.py
+++ Python/json2excel.py
@@ -1,20 +1,24 @@
 import pandas as pd 
 import json
 
+# User inputs ; .split() is used for creating a array from user input
 json_file = input("Enter the path of the json file : ")
 keys = input("Enter the keys to select separated by spaces : ").split()
 output_xlsx = input("Enter a name for the output file : ")
 specific_key = input("Is there a specific key to work on (y/n) : ")
 
+# Open and load the json document
 with open(json_file, 'r') as f:
     data = json.load(f)
 
+# Modify the data if we want to work with a specific key
 if specific_key == y:
-    main_key = input("Enter the main key to work on : ")
+    mainkey = input("Enter the main key to work on : ")
     certs = data[mainkey]
 else:
     certs = data
 
+# Load data into DataFrame (rows/col table) / create new variable with only the keys we want / export to .xlsx
 certs_df = pd.DataFrame(certs)
 certs_parsed = certs_df[keys]
 certs_parsed.to_excel(output_xlsx)
