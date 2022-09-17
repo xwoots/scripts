7ec7dec32f91f50ced075d261b92b728f8fd5089
xwoot <mateuw.coulibaly@gmail.com>
2022-09-17 11:50:07 +0000

    add the hosts entry

--- Bash/init_onprem_ubuntu.sh
+++ Bash/init_onprem_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add SSH Key to new host + change ip address & hostname
+# Add SSH Key to new host + change ip address & hostname + add host entry
 # Works only for on-prem hosts with Ubuntu 18.04+
 
 read -p "Entrez le nom d'utilisateur : " SSHUSER
@@ -12,6 +12,11 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub ${SSHUSER}@${SSHHOST}
 
 read -p "Entrez la nouvelle IP : " NEW_IP
 read -p "Entre le nouveau nom d'hôte : " NEW_HOSTNAME
-ssh -t mateuw@${TEMPLATE_IP} "sudo sed -i 's/${SSHHOST}\/24/${NEW_IP}\/24/' /etc/netplan/00-installer-config.yaml"
-ssh -t mateuw@${TEMPLATE_IP} "sudo hostnamectl set-hostname ${NEW_HOSTNAME}"
-ssh -t mateuw@${TEMPLATE_IP} "sudo netplan apply"
+
+ssh -t mateuw@${SSHHOST} "sudo sed -i 's/${SSHHOST}\/24/${NEW_IP}\/24/' /etc/netplan/00-installer-config.yaml \
+	&& sudo hostnamectl set-hostname ${NEW_HOSTNAME} \
+	&& sudo netplan apply"
+	
+echo ${NEW_IP} ${NEW_HOSTNAME} | sudo tee -a /etc/hosts
+
+
