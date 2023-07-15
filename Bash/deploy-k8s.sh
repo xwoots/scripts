0daf78c955238e10bfe39c223b11dd23e1577947
xwoots <57961128+xwoots@users.noreply.github.com>
2023-07-15 18:46:49 +0200

    Update deploy-k8s.sh

--- Bash/deploy-k8s.sh
+++ Bash/deploy-k8s.sh
@@ -35,7 +35,7 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
